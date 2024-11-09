ff1d6888e00713e5f01065b09331b49e57928821
batareyka_a <anastaish.batareyka@yandex.ru>
2024-11-09 11:06:01 +0300

    init project

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import js from "@eslint/js"
+import prettier from "eslint-config-prettier"
+export default [
+    js.configs.recommended,
+    prettier,
+    {
+        files: ["src/**/*.js"],
+        plugins: {
+            prettier: prettier.rules,
+        },
+        rules: {
+            "no-unused-vars": "warn",
+        },
+    }
+]
